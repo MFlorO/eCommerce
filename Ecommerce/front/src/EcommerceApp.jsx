353eec7d42e0e6a01ef4d66b02f143deb591705d
Flori O <floroldani@hotmail.com>
2023-03-22 23:46:33 -0300

    Crear AdminLayOut

--- Ecommerce/front/src/EcommerceApp.jsx
+++ Ecommerce/front/src/EcommerceApp.jsx
@@ -1,10 +1,12 @@
 import { AppTheme } from "~/theme";
 import { AppRouterPrincipal } from "~/router";
+import { CssBaseline } from '@mui/material';
 
 function EccomerceApp() {
   return (
     <div className="App">
       <AppTheme>
+        <CssBaseline />
         <AppRouterPrincipal />
       </AppTheme>
     </div>
