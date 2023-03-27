1540bfbfb9e649f68905c67f4da15ba3c5afa773
Flori O <floroldani@hotmail.com>
2023-03-27 18:42:38 -0300

    agregar barrel de editar categoria

--- Ecommerce/front/src/Admin/Page/Categorias/components/index.js
+++ Ecommerce/front/src/Admin/Page/Categorias/components/index.js
@@ -1,2 +1,3 @@
 export { default as CrearCategoria } from './CrearCategoria';
+export { default as EditCategoria } from './EditCategoria';
 export { default as ListaCategorias } from './ListaCategorias';
