15260e4f158025d85744e38cc8bb4a96850f2618
Flori O <floroldani@hotmail.com>
2023-04-06 18:35:33 -0300

    agregar padding

--- Ecommerce/front/src/Admin/Page/Categorias/Categorias.jsx
+++ Ecommerce/front/src/Admin/Page/Categorias/Categorias.jsx
@@ -21,7 +21,7 @@ const Categorias = () => {
     <Grid container spacing={3}>
 
     <Grid item xs={12} >
-       <Paper sx={{ display: "flex", flexDirection: "column", height: 'max-content', justifyContent:'center' }} p={{xs:'1', sm:'2'}}>
+       <Paper sx={{ p: 2 ,display: "flex", flexDirection: "column", height: 'max-content', justifyContent:'center' }}>
 
        <Stack sx={{flexDirection:"row"}}>
           <Button onClick={() => navigate(`/dashboard/admin/categorias/crearCategoria`) }><AddIcon />Crear Categoria</Button>
